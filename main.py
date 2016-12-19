f4f064901a477cfa86c45272e43d80aa46dd6dd5
Will <will.beddow@gmail.com>
2016-12-19 10:29:38 -0500

    W.I.L.L for telegram
    Rewritten and redone W.I.L.L as a telegram bot

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+class main():
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    main()
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#Standalone setup script for W.I.L.L
+if ! dpkg -s python-pip; then
+    echo "pip not installed, installing"
+    sudo apt-get install pip
+fi
+
+if ! dpkg -s git; then
+    echo "git not installed, installing"
+    sudo apt-get install git
+fi
+
+cd /usr/local
+
+echo "Downloading latest version of W.I.L.L"
+
+git clone "https://github.com/ironman5366/W.I.L.L-Telegram"
+
+cd W.I.L.L-telegram
+
+echo "Installing required python modules"
+
+sudo pip install -r reuqirements.txt
+
+echo "Setting up W.I.L.L as service"
+
+cp will.conf /etc/init/will.conf
+
+echo "Reloading services"
+
+sudo initctl reload-configuration
+
+echo "Finished installation. To start W.I.L.L, run sudo start will"
